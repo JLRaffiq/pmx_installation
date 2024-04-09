06e4d85394c6c6e6f7a2c009c600867eefd0c3fc
JLRaffiq <153411801+JLRaffiq@users.noreply.github.com>
2024-04-10 00:42:23 +0800

    pm_installation.sh

--- pm_installation.sh
+++ pm_installation.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Step 1: Update APT Cache
-sudo apt update
+apt update
 
 # Step 2: Configure Static IP Address
-sudo tee /etc/network/interfaces >/dev/null <<'EOF'
+tee /etc/network/interfaces >/dev/null <<'EOF'
 auto enp0s3
 iface enp0s3 inet static
         address 192.168.2.103/24
@@ -13,31 +13,31 @@ iface enp0s3 inet static
         gateway 192.168.2.1
         dns-nameservers 8.8.8.8
 EOF
-sudo systemctl restart NetworkManager
+systemctl restart NetworkManager
 
 # Step 3: Configure Hostname Resolution
-sudo hostnamectl set-hostname proxmox
+hostnamectl set-hostname proxmox
 exec bash
 echo "192.168.2.103  proxmox" | sudo tee -a /etc/hosts >/dev/null
 hostname
 hostname --ip-address
 
 # Step 4: Add the Proxmox VE Repository
-sudo apt install curl software-properties-common apt-transport-https ca-certificates gnupg2 -y
+apt install curl software-properties-common apt-transport-https ca-certificates gnupg2 -y
 echo "deb [arch=amd64] http://download.proxmox.com/debian/pve bookworm pve-no-subscription" | sudo tee /etc/apt/sources.list.d/pve-install-repo.list >/dev/null
 wget https://enterprise.proxmox.com/debian/proxmox-release-bookworm.gpg -O /etc/apt/trusted.gpg.d/proxmox-release-bookworm.gpg
-sudo apt update && sudo apt full-upgrade -y
+apt update && sudo apt full-upgrade -y
 
 # Step 5: Install the Proxmox Kernel
-sudo apt install proxmox-default-kernel -y
+apt install proxmox-default-kernel -y
 
 # Step 6: Install the Proxmox Packages
-sudo apt install proxmox-ve postfix open-iscsi chrony -y
+apt install proxmox-ve postfix open-iscsi chrony -y
 
 # Step 7: Remove the Linux Kernel
-sudo apt remove linux-image-amd64 'linux-image-6.1*' -y
-sudo update-grub
-sudo apt remove os-prober -y
+apt remove linux-image-amd64 'linux-image-6.1*' -y
+update-grub
+apt remove os-prober -y
 
 # Step 8: Reboot
-sudo reboot
+reboot
