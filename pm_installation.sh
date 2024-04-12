5de7379404f6f048a2e6db03792a4febe7551c76
JLRaffiq <153411801+JLRaffiq@users.noreply.github.com>
2024-04-12 20:10:40 +0800

    pm_installation.sh

--- pm_installation.sh
+++ pm_installation.sh
@@ -5,7 +5,7 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 #
-apt update
+apt -y update
 #
 ### Hostname-IP
 hostname=`hostname`
@@ -16,25 +16,25 @@ sed -i "/$hostname/c$ipadd\t$hostname" /etc/hosts
 hostname --ip-address
 #
 # Add the Proxmox VE Repository
-apt install curl software-properties-common apt-transport-https ca-certificates gnupg2 
+apt -y install curl software-properties-common apt-transport-https ca-certificates gnupg2 
 #
 echo "deb [arch=amd64] http://download.proxmox.com/debian/pve bookworm pve-no-subscription" > /etc/apt/sources.list.d/pve-install-repo.list
 #
 wget https://enterprise.proxmox.com/debian/proxmox-release-bookworm.gpg -O /etc/apt/trusted.gpg.d/proxmox-release-bookworm.gpg  
 #
-apt update && apt full-upgrade
+apt -y update && apt full-upgrade
 #
-apt apt install proxmox-default-kernel -y
+apt install proxmox-default-kernel -y
 #
-apt install proxmox-ve postfix open-iscsi chrony
+apt -y install proxmox-ve postfix open-iscsi chrony
 #
 ss -tunpl | grep 8006
 #
-apt remove linux-image-amd64 'linux-image-6.1*'
+apt -y remove linux-image-amd64 'linux-image-6.1*'
 #
 update-grub
 #
-apt remove os-prober
+apt -y remove os-prober
 #
 reboot
 #
